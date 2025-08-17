80229944f93038de8924bf4eceab3ce947756d5a
Weslley Campos <weslley_campos@outlook.com>
2025-08-17 20:20:09 -0300

    ♻️ refactor: Cleaned up imports and ensured consistent formatting across files.

--- composeApp/src/wasmJsMain/kotlin/br/com/weslleycampos/blog/App.kt
+++ composeApp/src/wasmJsMain/kotlin/br/com/weslleycampos/blog/App.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-
 import blog.composeapp.generated.resources.Res
 import blog.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun App() {
